10eaace2ac07cfa112c20fa7e3b530da05e314b8
Maksim Vaysbekker <mvaydev@gmail.com>
2026-06-05 19:30:54 +0800

    init: create project layout

---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<h1 class="text-3xl font-bold">Welcome to K1 Wallet</h1>
+<h2 class="text-2xl font-semibold">Pages</h2>
+<ul class="list-disc">
+	<li><a href="login" class="link-primary">Login</a></li>
+	<li><a href="staff" class="link-primary">Staff Dashboard</a></li>
+	<li><a href="student" class="link-primary">Student Dashboard</a></li>
+</ul>
